1ae1a3c5f3ee39a5779074a8ab98079c2f99c689
i.bergonyo <i.bergonyo_xsr@ereuse.org>
2018-07-11 22:19:03 +0200

    home_music

--- js/home_js.js
+++ js/home_js.js
@@ -12,9 +12,10 @@ function contacts() {
 	document.getElementById('music').style = "border-bottom-width:1px";
 
 }
+
 window.onload = function (){
 	document.getElementById('contacts').style = "border-bottom-width:0";
 	document.getElementById('music_groups').style.display = "none";
 	document.getElementById('music').onclick = music;
 	document.getElementById('contacts').onclick = contacts;
-}
+};
