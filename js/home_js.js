6737c611bf90f5bd5889107ea36b23933a2da8f0
Isardbp <>
2018-07-12 12:12:42 +0200

    pos oc.

--- js/home_js.js
+++ js/home_js.js
@@ -16,9 +16,25 @@ function contacts() {
     document.getElementById('music').style.color = "Black";
 }
 
+function scroll() {
+	var h = document.body.getBoundingClientRect().height;
+	var lim = h * 0.35 * 0.80;
+	console.log(document.documentElement.scrollTop);
+	console.log(document.documentElement.scrollTop >1);
+	if (document.body.scrollTop > lim || document.documentElement.scrollTop > lim){
+		console.log("aaaaaaa");
+		document.getElementById("menu").style.position = "fixed";
+	}
+	else{
+		document.getElementById("menu").style.position = "static";
+	}
+}
+
+
 window.onload = function (){
 	document.getElementById('contacts').style = "border-bottom-width:0";
 	document.getElementById('music_groups').style.display = "none";
 	document.getElementById('music').onclick = music;
 	document.getElementById('contacts').onclick = contacts;
+	window.onscroll = scroll;
 };
