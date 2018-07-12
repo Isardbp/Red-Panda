b28091ff349a5d88a288803ff0226cbae77d212f
i.bergonyo <i.bergonyo_xsr@ereuse.org>
2018-07-12 02:14:13 +0200

    home

--- js/home_js.js
+++ js/home_js.js
@@ -3,6 +3,8 @@ function music() {
 	document.getElementById('music_groups').style.display = "flex";
 	document.getElementById('music').style = "border-bottom-width:0";
 	document.getElementById('contacts').style = "border-bottom-width:1px";
+	document.getElementById('contacts').style.color = "Black";
+    document.getElementById('music').style.color = "#CB3131";
 }
 
 function contacts() {
@@ -10,7 +12,8 @@ function contacts() {
 	document.getElementById('cont_people').style.display = "flex";
 	document.getElementById('contacts').style = "border-bottom-width:0";
 	document.getElementById('music').style = "border-bottom-width:1px";
-
+	document.getElementById('contacts').style.color = "#CB3131";
+    document.getElementById('music').style.color = "Black";
 }
 
 window.onload = function (){
