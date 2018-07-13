7430b72576c9bb830ef362ccd1474673dc462c81
Isardbp <>
2018-07-13 11:56:00 +0200

    modal

--- js/home_js.js
+++ js/home_js.js
@@ -22,13 +22,45 @@ function scroll() {
 	console.log(document.documentElement.scrollTop);
 	console.log(document.documentElement.scrollTop >1);
 	if (document.body.scrollTop > lim || document.documentElement.scrollTop > lim){
-		console.log("aaaaaaa");
 		document.getElementById("menu").style.position = "fixed";
 	}
 	else{
 		document.getElementById("menu").style.position = "static";
 	}
 }
+
+function modal(){
+	var modal = document.getElementById('myModal');
+	var btn = document.getElementById('write');
+	var span = document.getElementsByClassName("close")[0];
+
+}
+
+
+function post_mss() {
+	var template= `<div class="mss">
+                <div class="header">
+                    <div class="header_img_name">
+                        <div class="center_img"><img src="fotos/perfil_2.png"></div>
+                        <div class="center_name"><p>Isardbp</p></div>
+                    </div>
+                    <div class="buttons_center">
+                        <button class="edit_but">Edit</button>
+                        <button class="delete_but">Delete</button>
+                    </div>
+                </div>
+                <div class="section"></div>
+                <div class="footer"></div>
+            </div>`
+
+
+
+}
+
+function press_Write(){
+	document.getElementById('myModal').classList.add('modal_emerge');
+}
+
 
 function press_contacts(){
 	var people = document.getElementsByClassName('people');
@@ -47,4 +79,23 @@ window.onload = function (){
 	}
 	window.onscroll = scroll;
 	press_contacts();
+
+	var modal = document.getElementById('modal');
+	var btn = document.getElementById('write');
+	var span = document.getElementsByClassName("close")[0];
+
+	btn.onclick = function(){
+		modal.style.display = "flex";
+		press_Write();
+
+	}
+	span.onclick = function(){
+		modal.style.display = "none";
+	}
+	window.onclick = function(event) {
+		if (event.target == modal){
+			modal.style.display = "none";
+		}
+	}
+
 };
